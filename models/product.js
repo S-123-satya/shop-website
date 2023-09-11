cfa968e8627468bf7f8397629c93cd7d5e9524fb
S-123-satya <satyaprakash5056742@gmail.com>
2023-09-11 16:12:22 +0530

    mongodb creates

--- models/product.js
+++ models/product.js
@@ -1,27 +1,69 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const { getdb } = require("../util/database");
 
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  url: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product{
+  constructor (title,price,description,imageUrl,id){
+    this.title=title;
+    this.price=price;
+    this.description=description;
+    this.imageUrl=imageUrl;
+    this._id=id;
   }
-});
+
+  save() {
+    const db =getdb()
+    let dbOp;
+    if(this._id){
+      //update
+
+    }
+    else{
+      dbOp=db.collection('products').insertOne(this)
+      .then(result=>console.log(result))
+      .catch(err=>console.log(err))
+    }
+  }
+  static fetchAll(){
+    const db =getdb()
+    return db.collection('products').find().toArray()
+    .then(result=>{
+      console.log(result)
+      return result;
+    })
+    .catch(err=>console.log(err))
+  }
+  static findById(prodId){
+    const db=getdb()
+    return db.collection('products').find({_id:new mongodb.ObjectId(prodId)})
+    .next()
+    .then(product=>{
+      console.log(product);
+      return product
+    })
+    .catch(err=>console.log(err))
+  }
+}
+
+// const Product = sequelize.define('product', {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true
+//   },
+//   title: Sequelize.STRING,
+//   price: {
+//     type: Sequelize.DOUBLE,
+//     allowNull: false
+//   },
+//   imageUrl: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   },
+//   description: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   }
+// });
 
 module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,8 +1,27 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;//mongoclient constructor
 
-const sequelize = new Sequelize('node-complete', 'root', 'Satya0*123', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://vikas_5537:h2o9BctI1xVfbZp6@cluster0.nqyoptf.mongodb.net/?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db=client.db();
+      callback(client);
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
 
-module.exports = sequelize;
+const getdb=()=>{
+  if(_db)
+    return _db
+  throw "NO such database exist";
+}
+
+exports.mongoConnect=mongoConnect;
+exports.getdb=getdb;
